d9de4581a7f965be890347c55046b8b2b8ee0069
CanilMedvedBielyy <lulubonini@hotmail.com>
2026-08-21 13:45:08 -0300

    Update getStore call to include siteID and token

--- netlify/functions/get-orders.js
+++ netlify/functions/get-orders.js
@@ -11,7 +11,7 @@ exports.handler = async function (event) {
   }
 
   try {
-    const store = getStore('orders');
+    const store = getStore('orders', { siteID: process.env.BLOBS_SITE_ID, token: process.env.BLOBS_ACCESS_TOKEN });
     const { blobs } = await store.list();
 
     const orders = await Promise.all(
